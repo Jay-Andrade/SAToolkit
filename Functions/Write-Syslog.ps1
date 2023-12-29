e96a2ba925d6abe56e84f91958132bb6b7a9c520
Jay <47705155+Jay-Andrade@users.noreply.github.com>
2023-12-29 15:25:27 -0500

    Add trim, remove logging options

--- Functions/Write-Syslog.ps1
+++ Functions/Write-Syslog.ps1
@@ -15,41 +15,75 @@ The log file to output the log entry to. Defaults to: "$env:temp\syslog.log"
 Displays the source of the log entry in the entry. Important for log entries to Rebootflag.log
 
 .EXAMPLE
-Write-Syslog "Test message" "INFO" "C:\SAToolkit\example.log" "- AMP - AMP NAME" -displayMessage
+Write-Syslog "INFO" "Test message" "C:\SAToolkit\example.log" 
 This will write a message to syslog with catagory 'INFO'. It will display the source of the log entry and will write the log entry to console.
 
 .EXAMPLE
-Write-Syslog "Test message" "WARN"
+Write-Syslog "WARN" "Test message"
 This will write a message to syslog with catagory 'WARN'. It will use the default output directory "$env:temp\syslog.log"
-It will not show the source of the log entry, and will not write the log entry to console.
 #>
+
 function Write-Syslog {
     Param (
+
         [Parameter(Mandatory = $TRUE, Position = 0)]
-        $message,
+        $Category,
 
         [Parameter(Mandatory = $TRUE, Position = 1)]
-        $category,
+        $Message,
 
         [Parameter(Position = 2)]
-        $outputFile = "$env:temp\syslog.log",
-
-        [Switch]$displayMessage,
-        
-        [Switch]$displaySource
+        $OutputFile = "$env:temp\syslog.log"
     )
     
-    if ($displaySource){
-        $string = "["+(Get-Date -Format g)+"] " + "[$category] [SOURCE] $source [MESSAGE] $message"
-    } else {
-        $string = "["+(Get-Date -Format g)+"] " + "[$category] [MESSAGE] $message"
+    switch ($Category) {
+        'ERROR' { $etype = 1; break}
+        'WARN' { $etype = 2; break}
+        default { $etype = 4; break}
     }
 
-    if ($displayMessage) {
-        Write-Host $string
-    }
+    $logSize = (Get-ItemProperty $OutputFile).Length/1MB
+    if ($logSize -gt 1.0) {
+        #Cannot call Write-Syslog normally for logging as function will infinetly recurse
+        $dateThreshold = (Get-Date).AddDays(-30)
 
-    Out-File -FilePath $outputFile -InputObject $string -Append
+        $filteredEntries = Get-Content $OutputFile | Where-Object {
+            try {
+                # Extract the timestamp and trim any additional whitespace or hidden characters
+                $dateString = $_.Split(']')[0].Trim('[', ']', ' ', "`t", "`n", "`r")
+                # Specify the format of the date string accommodating single and double-digit days and months
+                $entryDate = [DateTime]::ParseExact($dateString, "M/d/yyyy h:mm tt", [Globalization.CultureInfo]::InvariantCulture)
+        
+                # Keep the entry if it's newer than the threshold
+                $entryDate -ge $dateThreshold
+            }
+            catch {
+                # Handle entries with invalid date formats. Setting var to avoid numerous duplicate writes.
+                $invalidEntries = $TRUE
+            }
+        }
+
+        #Write filtered log entries (those newer than 30 days) to output file
+        #Has to come before the $parsingMessage and $invalidEntries to prevent overwriting those items
+        $filteredEntries | Out-File $OutputFile
+        
+        $parsingMessage = "["+(Get-Date -Format g)+"] " + "[INFO] [SOURCE] $source [MESSAGE] Trimmed syslog file: $OutputFile - Keeping last 30 days of logs."
+        Write-Host $parsingMessage
+        Out-File -FilePath $OutputFile -InputObject $parsingMessage -Append
+
+        if ($invalidEntries) {
+            $errorMessage = "["+(Get-Date -Format g)+"] " + "[ERROR] [SOURCE] $source [MESSAGE] Skipped log entries with invalid date format during trim."
+            Write-Host $errorMessage
+            Out-File -FilePath $OutputFile -InputObject $errorMessage -Append
+        }
+        
+    }
+    
+    $string = "["+(Get-Date -Format g)+"] " + "[$Category] [SOURCE] $source [MESSAGE] $Message"
+
+    Write-Host $string
+
+    Out-File -FilePath $OutputFile -InputObject $string -Append
     Return
 }
 Set-Alias -Name 'syslog' -Value 'Write-Syslog'
